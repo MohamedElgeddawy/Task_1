d423f481778e946ed21ad7f9107b00fb42653ff7
Mohamed Eslam <58227911+Mo77797@users.noreply.github.com>
2021-10-24 20:29:20 +0200

    Initial commit

---
+++ codingdiagnostic/buyLotsOfFruit.py
@@ -0,0 +1,51 @@
+# buyLotsOfFruit.py
+# -----------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+To run this script, type
+
+  python buyLotsOfFruit.py
+
+Once you have correctly implemented the buyLotsOfFruit function,
+the script should produce the output:
+
+Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
+"""
+from __future__ import print_function
+
+fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
+               'limes': 0.75, 'strawberries': 1.00}
+
+
+def buyLotsOfFruit(orderList):
+
+    ''' takes a list of(fruit,pound)tuples and returns the cost of your list. If there is some fruit in
+        the list which doesn’t appear in fruitPrices it should print an error message and return None'''
+    totalCost = 0.0
+    for item , num  in orderList:
+        if item in fruitPrices:
+            totalCost += num * fruitPrices[item]
+
+        else:
+            print('Enter right fruit name and number of it ')
+            return  None
+
+    return totalCost
+        
+
+# Main Method
+if __name__ == '__main__':
+    "This code runs when you invoke the script from the command line"
+    orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
+    print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
